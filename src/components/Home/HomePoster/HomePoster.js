1774821ba4529b55c8412a494dd282e0a8d5652f
NasirBaigIT <Nasirbaig428@gmail.com>
2024-05-26 14:43:49 +0500

    updating main banner

--- src/components/Home/HomePoster/HomePoster.js
+++ src/components/Home/HomePoster/HomePoster.js
@@ -1,9 +1,20 @@
 import React from "react";
 import styles from "./HomePoster.scss";
 
+import left from "../../../assets/homePoster/leftBar.png";
+import right from "../../../assets/homePoster/rightBar.png";
+import main from "../../../assets/homePoster/topMask.png";
+
 const HomePoster = () => {
   return (
     <div className="expert-insights">
+      <div className="image-section">
+        <div className="image-stack">
+          <img src={left} alt="Image 1" className="stacked-image" />
+          <img src={main} alt="Image 2" className="stacked-image" />
+          <img src={right} alt="Image 3" className="stacked-image" />
+        </div>
+      </div>
       <div className="text-section">
         <h1>
           Your <span className="highlight">Trusted</span> Partner for{" "}
@@ -17,9 +28,6 @@ const HomePoster = () => {
           <button className="need-expert">Need An Expert</button>
           <button className="become-expert">Become An Expert</button>
         </div>
-      </div>
-      <div className="image-section">
-        {/* <img src="/src/assets/homePoster/topMask.png" alt="Expert Insights" /> */}
       </div>
     </div>
   );
