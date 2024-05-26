ee59405d536175f34596e968a402389c32f775a5
NasirBaigIT <Nasirbaig428@gmail.com>
2024-05-26 23:11:24 +0500

    updating mobile design

--- src/components/Home/Offers/offers.js
+++ src/components/Home/Offers/offers.js
@@ -10,7 +10,7 @@ import banner3 from "../../../assets/offers/banner3.png";
 
 const Offers = () => {
   return (
-    <section className="flex justify-center items-center px-16 py-16 mt-52 w-full bg-indigo-900 max-md:px-5 max-md:mt-10 max-md:max-w-full">
+    <section className="flex justify-center items-center px-16 py-16 w-full bg-indigo-900 max-md:px-5 max-md:max-w-full">
       <div className="flex flex-col items-center w-full max-w-[1120px] max-md:max-w-full">
         <h1 className="text-5xl font-medium leading-10 text-center text-white capitalize">
           What we offer
