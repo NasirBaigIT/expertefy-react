c5d07ea99ee5d89808271ea1a37aaa2159e21f1d
NasirBaigIT <Nasirbaig428@gmail.com>
2024-06-04 00:35:11 +0500

    splash screen feedback

--- src/components/SplashScreen/SplashScreen.js
+++ src/components/SplashScreen/SplashScreen.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./SplashScreen.css"; // Import the CSS file for styling
 
 import loadingGif from "../../assets/loader/test2.gif";
 
 const SplashScreen = () => {
+  const imagesRef = useRef();
+  const divRef = useRef();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      imagesRef.current.src = null;
+      divRef.current.style.backgroundColor = "white";
+    }, 2350); // Change the timeout duration as needed
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <div className="splash-screen">
-      <img src={loadingGif} alt="Loading" className="splash-gif" />
+    <div className="splash-screen" ref={(el) => (divRef.current = el)}>
+      <img
+        src={loadingGif}
+        alt=""
+        className="splash-gif"
+        ref={(el) => (imagesRef.current = el)}
+      />
     </div>
   );
 };
